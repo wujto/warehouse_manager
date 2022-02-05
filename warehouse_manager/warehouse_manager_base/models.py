c97be7de07855cb0d4a1fc911a55bc11d912c507
Patryk Wójtowicz <patrix9811@gmail.com>
2022-02-05 18:34:05 +0100

    Add ProductSet, Localization, Category and Product

--- warehouse_manager/warehouse_manager_base/models.py
+++ warehouse_manager/warehouse_manager_base/models.py
@@ -1,13 +1,84 @@
 from django.db import models
 
+class ProductSetModel(models.Model):
+    id_number = models.CharField(max_length=9, unique=True, blank=False, null=False)
+    name = models.CharField(max_length=25, blank=False, null=False)
+    description = models.CharField(max_length=100, default="")
+    products_count = models.SmallIntegerField(default=0)
+
+    def __str__(self):
+        return f'{self.id_number} {self.name}'
+
+    def get_info(self):
+        info ={'id_number': self.id_number,
+        "name": self.name,
+        "description":self.description,
+        'products_count': self.products_count,
+        }
+
+        return info
+
+    def edit_name(self, name):
+        try:
+            self.name = name
+        except:
+            return False
+        return True
+
+    def edit_description(self, description):
+        self.description = description
+
+class LocalizationModel(models.Model):
+    name = models.CharField(unique=True, max_length=25, blank=False, null=False)
+    description = models.CharField(max_length=50, default= "")
+
+    def __str__(self):
+        return self.name
+    
+    def get_description(self):
+        return self.description
+
+    def edit_name(self, name):
+        # if len(name) == 0:
+        #     raise Exception("Localization name must be longer!!")
+
+        try:
+            self.name = name
+        except:
+            return False
+        return True
+
+    def edit_description(self, description):
+        self.description = description
+
+class CategoryModel(models.Model):
+    name = models.CharField(unique=True, max_length=15, blank=False, null=False)
+    description = models.CharField(max_length=100, defalut="")
+
+    def __str__(self):
+        return self.name
+
+    def get_description(self):
+        return self.description
+    
+    def edit_description(self, description):
+        self.description = description
+
+    def edit_name(self, name):
+        try:
+            self.name = name
+        except:
+            return False
+        return True
+
 class ProductModel(models.Model):
     id_number = models.CharField(unique=True, max_length=9) #Number to identify product in database
     name = models.CharField(max_length=25, blank=False, null=False)
     description = models.CharField(max_length=100, default="")
-    category = models.ForeignKey("CategoryModel", on_delete= models.DO_NOTHING, blank=False, null=False)
-    localization = models.ForeignKey("LocalizationModel", on_delete = models.DO_NOTHING, blank=False, null=False)
+    category = models.ForeignKey(CategoryModel, on_delete= models.DO_NOTHING, blank=False, null=False)
+    localization = models.ForeignKey(LocalizationModel, on_delete = models.DO_NOTHING, blank=False, null=False)
     photo = models.FileField()
-    product_set = models.ForeignKey("ProductSetModel",on_delete = models.DO_NOTHING, null=True, blank=False)
+    product_set = models.ForeignKey(ProductSetModel,on_delete = models.DO_NOTHING, null=True, blank=False)
     product_user = models.ForeignKey("User",on_delete = models.DO_NOTHING, blank=False, null=True)
 
     def __str__(self):
@@ -26,3 +97,19 @@ class ProductModel(models.Model):
         }
 
         return info
+
+# Edit name, description, category, localization
+    def edit_info(self, info):
+        self.name = info['name']
+        self.description = info['description']
+        self.category = info['category']
+        self.localization = info['localization']
+
+    def set_photo(self, photo):
+        self.photo = photo
+
+    def set_product_set(self, product_set):
+        self.product_set = product_set
+
+    def set_product_user(self, user):
+        self.product_user = user
