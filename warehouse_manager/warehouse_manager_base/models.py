5651a40ed91b7bf7b9a7b8083dd4b6e67bc7da18
Patryk Wójtowicz <patrix9811@gmail.com>
2022-02-06 13:42:49 +0100

    Add CustomeUserManager

--- warehouse_manager/warehouse_manager_base/models.py
+++ warehouse_manager/warehouse_manager_base/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.contrib.auth.base_user import BaseUserManager
+from django.contrib.auth.models import AbstractBaseUser
 
 class ProductSetModel(models.Model):
     id_number = models.CharField(max_length=9, unique=True, blank=False, null=False)
@@ -113,3 +115,26 @@ class ProductModel(models.Model):
 
     def set_product_user(self, user):
         self.product_user = user
+
+
+class CustomeUserManager(BaseUserManager):
+    def create_user(self, email, password, **extra_fields):
+        if not email:
+            raise ValueError("The email must be set")
+        email = self.normalize_email(email)
+        user = self.model(email = email, **extra_fields)
+        user.set_password(password)
+        user.save()
+        return user
+
+    def create_superuser(self, email, password, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('is_active', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff = True')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser = True')
+
+        return self.create_user(email, password, **extra_fields)
