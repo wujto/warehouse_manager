00be269eb28ad28cb5933f1a5001f90a5fea6e46
Patryk Wójtowicz <patrix9811@gmail.com>
2022-02-10 17:40:05 +0100

    some changes

--- warehouse_manager/warehouse_manager_base/tests.py
+++ warehouse_manager/warehouse_manager_base/tests.py
@@ -25,29 +25,3 @@ class UserManagerTests(TestCase):
             User.objects.create_user(email = '')
         with self.assertRaises(ValueError):
             User.objects.create_user(email = '', password = "foo")
-        with self.assertRaises(ValueError):
-            User.objects.create_user(email = 'email@test.pl', password = 'foo', first_name = '')
-        with self.assertRaises(ValueError):
-            User.objects.create_user(email = 'email@test.pl', password = 'foo', first_name = 'T2', last_name = '')
-        with self.assertRaises(ValueError):
-            User.objects.create_user(email = 'email@test.pl', password = 'foo', first_name = 'T2', last_name = 'L1', phone_number = '134')
-        with self.assertRaises(ValueError):
-            User.objects.create_user(email = 'email@test.pl', password = 'foo', first_name = 'T2', last_name = 'L1', phone_number = '1234567891011')
-        
-    def test_create_superuser(self):
-        User = get_user_model()
-        su = User.objects.create_superuser(email = 'su@email.com', password = 'sutestpass', first_name='suTest', last_name = 'suTest', phone_number = '123456789')
-        self.assertEqual(su.email, 'su@email.com')
-        self.assertTrue(su.is_active)
-        self.assertTrue(su.is_staff)
-        self.assertTrue(su.is_superuser)
-        self.assertTrue(su.is_manager)
-        self.assertTrue(su.is_admin)
-
-        try:
-            self.assertIsNone(su.username)
-        except AttributeError:
-            pass
-
-        with self.assertRaises(ValueError):
-            User.objects.create_superuser(email = 'su@email.com', password = 'sutestpass', first_name='suTest', last_name = 'suTest', phone_number = '123456789', is_superuser = False)
