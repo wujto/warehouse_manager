4a566d9f93b6fda1a9576f1c6fdef38692d7ebbb
Patryk Wójtowicz <patrix9811@gmail.com>
2022-05-29 18:39:02 +0200

    29.25.2022

--- warehouse_manager/warehouse_manager_browser_app/urls.py
+++ warehouse_manager/warehouse_manager_browser_app/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path, include
-from .views import ProfileView, UsersListView, WarehouseListView
+from .views import ProfileView, UsersListView, WarehouseListView, ProductDetailsView
 
 urlpatterns = [
     path('accounts/', include('django.contrib.auth.urls')),
     path('', ProfileView.as_view(), name = 'profile'),
+    path('product/<pk>',ProductDetailsView.as_view(), name = 'product_detail'),
     path('users', UsersListView.as_view(), name = 'users'),
     path('warehouse', WarehouseListView.as_view(), name = 'warehouse')
 ]
--- warehouse_manager/warehouse_manager_browser_app/views.py
+++ warehouse_manager/warehouse_manager_browser_app/views.py
@@ -11,10 +11,18 @@ from warehouse_manager_base.models import ProductModel, CategoryModel, Localizat
 class ProfileView(LoginRequiredMixin, TemplateView):
     template_name = 'logged_in/base.html'
 
-class UsersListView(LoginRequiredMixin, ListView):
-    model = CustomUserModel
+class UsersListView(LoginRequiredMixin, TemplateView):
     template_name = 'logged_in/users_list.html'
+
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(*args, **kwargs)
+        context['object_list'] = CustomUserModel.objects.all()
+        return context
 
 class WarehouseListView(LoginRequiredMixin, ListView):
     queryset = ProductModel.objects.filter(localization = 1)
     template_name = 'logged_in/warehouse_list.html'
+
+class ProductDetailsView(LoginRequiredMixin, DetailView):
+    model = ProductModel
+    template_name = 'logged_in/details.html'
