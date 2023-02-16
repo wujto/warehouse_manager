30c9faa76291a042248e9e2157e0bb387e5d1a47
wujto <79266036+wujto@users.noreply.github.com>
2023-02-16 19:25:40 +0100

    Update settings.py

--- warehouse2/warehouse2/settings.py
+++ warehouse2/warehouse2/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-iu^#nd8fxxj)d##f)68_6)@3#8e8fxi34mb0wp(b(&752%+50)'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
